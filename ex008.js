e0e7881fd94ae6aa85ba18fed63573efcdfcfd3f
flaviosegantini <flaviosegantini@gmail.com>
2022-10-06 15:39:27 -0300

    js atualizado

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 150.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`dirija sempre usando o cinto de segurança!`)
